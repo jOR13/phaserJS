c926665acf7251f31c6d9be59cfdb82f8e16ed14
Jesús Ochoa Rabelo <ochoarj13@hotmail.com>
2018-10-22 11:12:25 -0600

    Update src/GamePlay.js

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -4,20 +4,51 @@ GamePlayManager = {
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.scale.pageAlignHorizontally=true;
         game.scale.pageAlignVertically=true;
+        
+        this.flagFirstMouseDown =false;
     },
     preload: function() {
         game.load.image('background', 'assets/images/background.png');
         game.load.spritesheet('horse', 'assets/images/horse.png',84,156,2);
+        game.load.spritesheet('diamonds', 'assets/images/diamonds.png',81,84,4);
     },
     create: function() {
         game.add.sprite(0,0,'background');
         this.horse = game.add.sprite(0,0,'horse');
         this.horse.frame=0;
-        this.horse.x = game.width/2;
-        this.horse.y = game.heigth/2;
+        this.horse.x = game.width*.5;
+        this.horse.y = game.height*.5;
+        this.horse.anchor.setTo(0.5);
+        
+        game.input.onDown.add(this.onTap, this);
     },
+    
+    onTap: function(){
+        
+    this.flagFirstMouseDown = true;
+    
+    },
+    
     update: function() {
-        console.log("update");
+       if(this.flagFirstMouseDown){
+        var pointerX = game.input.x;
+        var pointerY = game.input.y;
+        
+        var distX = pointerX - this.horse.x;
+        var distY = pointerY - this.horse.y;
+        
+        if(distX>0){
+            this.horse.scale.setTo(1,1);
+        }else{
+            
+            this.horse.scale.setTo(-1,1);
+        }
+        
+        
+        this.horse.x += distX * .04;
+        this.horse.y += distY * .04;
+        this.horse.y += distY * .04;
+       }
     }
 }
 
